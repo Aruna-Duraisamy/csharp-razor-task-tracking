c18b8edd31de0d36870c3a8c78083c4afec0f611
aruna-duraisamy <68436256+Aruna-Duraisamy@users.noreply.github.com>
2021-10-11 22:24:37 +0100

    Fix

--- Tasker/Pages/Index.cshtml.cs
+++ Tasker/Pages/Index.cshtml.cs
@@ -20,8 +20,7 @@ namespace Tasker.Pages
 
         public void OnGet()
         {
-            Tasks = _context.Tasks
-                .OrderBy(x => x.Priority);
+            Tasks = _context.Tasks.OrderBy(x => x.Priority);
         }
     }
 }
